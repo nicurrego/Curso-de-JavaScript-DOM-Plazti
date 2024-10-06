dea86c60bc7880357cfed32f926ab5d13537e0e2
Nicolas Urrego <nicurrego@gmail.com>
2024-10-06 14:46:22 +0900

    Navegación efectiva entre nodos // Clase #5

--- sandBox.js
+++ sandBox.js
@@ -15,3 +15,47 @@
 // document.querySelectorAll("element") => trae un **Lista de Nodos** que contengan el mismo selector
 
 //getElement es mas veloz que querySelector*******
+
+// const parent = document.getElementById("parent")
+// console.log(parent);
+
+// const children = parent.children;
+// console.log(children);
+
+// const firstChild = parent.firstElementChild;
+// console.log(firstChild);
+
+// const lastChild = parent.lastElementChild;
+// console.log(lastChild);
+
+// const previousSibling = parent.previousElementSibling;
+// console.log(previousSibling);
+
+// const nextSibling = parent.nextelementSibling;
+// console.log(nextSibling);
+
+// children ; // live HTMLCollection
+// childNodes; // live Nodelist
+// firstChild; // live Nodelist 
+// firstElementChild; // non-live HTMCollection
+// lastChild; // live NodeList
+// lastElementChild; // non-live HTMLCollection
+// previousSibling; // live NodeList
+// previousElementSibling; // non-live HTMLCollection
+// nextSibling; // live NodeList
+// nextElementSibling; // non-liveHTMLCollection
+
+
+const children2 = document.querySelector('li')
+console.log(children2);
+
+const parent2 = children2.parentNode;
+console.log(parent2);
+
+const grandParent = children2.parentElement;
+console.log(grandParent);
+
+const grandGrandParent = children2.closest("menu"); // este último es el más utilizado
+console.log(grandGrandParent);
+
+
