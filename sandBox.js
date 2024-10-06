c47f79e9f2e8d460a7f34be5c280562c82ca5642
Nicolas Urrego <nicurrego@gmail.com>
2024-10-06 14:03:11 +0900

    ¿Qué es el DOM y Window Object? // Clase #2

---
+++ sandBox.js
@@ -0,0 +1,4 @@
+// El DOM es para Document Object Model
+
+// es diferente al Window, que vendría siendo como el padre del DOM.
+// El DOM se puede tomar como el contenido de los archivos que trabajamos.
