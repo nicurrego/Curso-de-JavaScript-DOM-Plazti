3f2f29adad026ce45e21239379ad970e5ea46fbb
Nicolas Urrego <nicurrego@gmail.com>
2024-10-06 14:27:50 +0900

    Consultando el DOM con getElementById y querySelector // Clase #4

--- sandBox.js
+++ sandBox.js
@@ -4,3 +4,14 @@
 // El DOM se puede tomar como el contenido de los archivos que trabajamos.
 
 // console.dir(document) => muestra el DOM en la consola, junto con las propiedades disponibles que nos da el window.
+
+// document.getElementById("element") => trae el elemento indicado
+// document.querySelector("element") => trae el primer elemento que cumpla con lo demandado
+
+// document.getElementsByClassName("menu-items") => trae un arreglo de elementos HTML
+
+// document.getElementsByTagName("p") => trae todos los elementos del mismo tag
+
+// document.querySelectorAll("element") => trae un **Lista de Nodos** que contengan el mismo selector
+
+//getElement es mas veloz que querySelector*******
