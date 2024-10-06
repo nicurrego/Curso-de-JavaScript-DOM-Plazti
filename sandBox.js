b11f5cd5f56a6ae3ed81dbddfa368669048799f9
Nicolas Urrego <nicurrego@gmail.com>
2024-10-06 14:13:40 +0900

    Explorando Nodos del DOM // Clase #3

--- sandBox.js
+++ sandBox.js
@@ -2,3 +2,5 @@
 
 // es diferente al Window, que vendría siendo como el padre del DOM.
 // El DOM se puede tomar como el contenido de los archivos que trabajamos.
+
+// console.dir(document) => muestra el DOM en la consola, junto con las propiedades disponibles que nos da el window.
